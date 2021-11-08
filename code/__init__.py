893229f3299a2d841c483675d315bd95e1f0a7cb
Mats van Tongeren <mats.van.tongeren@gmail.com>
2021-11-08 17:40:13 +0100

    Add setup

---
+++ code/__init__.py
@@ -0,0 +1,2 @@
+from .functions import main
+__version__ = '0.0'
---
+++ code/functions.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+def main():
+    """
+    Do something with the parameters
+    
+    Parameters
+    ------------
+    
+    X : data
+    
+    y : labels
+    
+    Returns
+    ------------
+    
+    beta: np.ndarray
+    
+    
+    """
+    print("Goodbye World")
+
+if __name__ == '__main__':
+    main()
