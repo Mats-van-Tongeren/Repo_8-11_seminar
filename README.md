39356b9a4dd33bce840223ca928b86aa95849afb
test_package GitHub Action <test_package@github.com>
2021-11-26 18:53:52 +0000

    Update documentation

--- README.md
+++
@@ -1,1 +0,0 @@
-If this readme was meant to be read, surely it would have more to say.
--- linear_regression/__init__.py
+++
@@ -1,4 +0,0 @@
-__version__ = '0.01'
-
-from .regression import least_squares  # noqa
-from .dumb import StupidFunction 
